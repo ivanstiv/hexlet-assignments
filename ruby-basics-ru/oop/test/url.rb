badc84a84e996b1843dd7f9ef84d1db4fc40d404
Ivan Titov <85026038+ivanstiv@users.noreply.github.com>
2025-05-09 11:33:46 +0300

    url.rb

--- ruby-basics-ru/oop/test/url.rb
+++ ruby-basics-ru/oop/test/url.rb
@@ -1,52 +1,38 @@
 # frozen_string_literal: true
 
 # BEGIN
-require 'forwardable'
 require 'uri'
 
 class Url
-	extend Forwardable
-    include Comparable
+  attr_reader :scheme, :host, :port, :query_params
 
-  def initialize(url_string)
-    @uri = URI.parse(url_string)
-    @query_params = parse_query_params(@uri.query)
-  end
-
-  def_delegators :@uri, :scheme, :host, :port
-
-  def query_params
-    @query_params.dup.freeze
+  def initialize(url)
+    uri = URI.parse(url)
+    @scheme = uri.scheme
+    @host = uri.host
+    @port = uri.port
+    
+    @query_params = {}
+    if uri.query
+      uri.query.split('&').each do |pair|
+        key, value = pair.split('=')
+        @query_params[key.to_sym] = value
+      end
+    end
   end
 
   def query_param(key, default_value = nil)
     @query_params.fetch(key, default_value)
   end
 
- def <=>(other)
-    return nil unless other.is_a?(Url)
-
-    comparison = [
-      scheme <=> other.scheme,
-      host <=> other.host,
-      port <=> other.port,
-      query_params <=> other.query_params
-    ]
-
-    comparison.all?(&:zero?) ? 0 : nil
-  end
-
-  private
-
-  def parse_query_params(query_string)
-    return {} if query_string.nil? || query_string.empty?
-
-    query_string.split('&').each_with_object({}) do |pair, hash|
-      key, value = pair.split('=', 2)
-      hash[key] = "value"
-    end
+  def ==(other)
+    return false unless scheme == other.scheme && host == other.host && port == other.port
+    query_params == other.query_params
   end
 end
+
+  
+   
 
 
 # END
